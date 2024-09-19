4c66303d20d31f7adc0a71321bc55e0307e162a8
JordanSerafini <jordan@solution-logique.fr>
2024-09-19 15:06:05 +0200

    pagesjaunes

--- js/pages_jaunes.js
+++ js/pages_jaunes.js
@@ -25,7 +25,7 @@ const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
         await page.type('#ou', 'Annecy');
         await delay(500);
 
-        await page.type('#quoiqui', 'hotel');
+        await page.type('#quoiqui', 'restaurant');
         await delay(500);
 
         await page.waitForSelector('#findId', { visible: true });
@@ -47,22 +47,53 @@ const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
 
         console.log('Name extraits :', name);
 
-        
+
         const addresses = await page.evaluate(() => {
             const addressElements = Array.from(document.querySelectorAll('a[title="Voir le plan"]'));
-        
+
             return addressElements.map(el => {
                 const textNodes = Array.from(el.childNodes).filter(node => node.nodeType === Node.TEXT_NODE);
                 return textNodes.map(node => node.textContent.trim()).join(' ');
             });
         });
-        
+
         console.log('Adresses extraites :', addresses);
+
+
+        await page.evaluate(() => {
+            const button = Array.from(document.querySelectorAll('span.value'))
+                .find(span => span.innerText.includes('Afficher le N°'));
+
+            if (button) {
+                button.click();
+                console.log('Clic sur le bouton "Afficher le N°" réussi.');
+            } else {
+                console.log('Bouton "Afficher le N°" non trouvé.');
+            }
+        });
+
+
+
+        const numeros = await page.evaluate(() => {
+            const annonceurElements = document.querySelectorAll('span.annonceur');
+            
+            return Array.from(annonceurElements).map(el => el.innerText.trim());
+        });
         
+        console.log('Numéros extraits :', numeros);
+        
+        const data = name.map((nom, index) => ({
+            name: nom,
+            address: addresses[index] || 'Adresse non trouvée',
+            phone: numeros[index] || 'Numéro non trouvé'
+        }));
+
+        console.log('Données JSON assemblées :', JSON.stringify(data, null, 2));
+
 
     } catch (error) {
         console.error('Erreur dans le processus :', error);
-        
+
         // if (error instanceof puppeteer.errors.TimeoutError) {
         //     const screenshotPath = 'error_screenshot.png';
         //     await page.screenshot({ path: screenshotPath, fullPage: true });
